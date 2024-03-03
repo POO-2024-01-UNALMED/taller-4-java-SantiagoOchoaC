94301bb9e0cb65719cfa757244914328969bf8bf
SantiagoOchoaC <159485469+SantiagoOchoaC@users.noreply.github.com>
2024-03-02 19:27:40 -0500

    Corrección Clase Tipo

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -4,9 +4,11 @@ public enum Tipo {
 	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
 	
 	private int codigo;
-	private String nombre;
-	public Tipo(int codigo,String nombre) {
+	
+	Tipo(int codigo) { // Cambiamos la visibilidad y eliminamos el otro parametro que no es necesario. 
 		this.codigo = codigo;
-		this.nombre = nombre;
+
 	}
+	
+
 }
